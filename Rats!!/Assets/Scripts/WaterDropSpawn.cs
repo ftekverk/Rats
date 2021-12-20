7696a73b9352dad8942d88a0797f9eb5189f325d
ksui01 <suistem5@gmail.com>
2021-12-20 18:18:02 -0500

    waterdrops working but doesn't kill

--- Rats!!/Assets/Scripts/WaterDropSpawn.cs
+++ Rats!!/Assets/Scripts/WaterDropSpawn.cs
@@ -10,6 +10,10 @@ public class WaterDropSpawn : MonoBehaviour
     public Transform spawnPoint3;
     public Transform spawnPoint4;
     public Transform spawnPoint5;
+    public Transform spawnPoint6;
+    public Transform spawnPoint7;
+    public Transform spawnPoint8;
+    public Transform spawnPoint9;
 
     public float spawnRangeStart = 0.5f;
     public float spawnRangeEnd = 1.2f;
@@ -30,12 +34,21 @@ public class WaterDropSpawn : MonoBehaviour
 
     void spawnTree()
     {
-        int SPnum = Random.Range(1, 5);
-        if (SPnum == 1) { spawnPoint = spawnPoint1; }
-        else if (SPnum == 2) { spawnPoint = spawnPoint2; }
-        else if (SPnum == 3) { spawnPoint = spawnPoint3; }
-        else if (SPnum == 4) { spawnPoint = spawnPoint4; }
-        else if (SPnum == 5) { spawnPoint = spawnPoint5; }
-        Instantiate(dropPrefab, spawnPoint.position, Quaternion.identity);
+        //int SPnum = Random.Range(1, 2);
+        //if (SPnum == 1) { spawnPoint = spawnPoint1; }
+        //else if (SPnum == 2) { spawnPoint = spawnPoint2; }
+        //else if (SPnum == 3) { spawnPoint = spawnPoint3; }
+        //else if (SPnum == 4) { spawnPoint = spawnPoint4; }
+        //else if (SPnum == 5) { spawnPoint = spawnPoint5; }
+
+        Instantiate(dropPrefab, spawnPoint1.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint2.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint3.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint4.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint5.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint6.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint7.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint8.position, Quaternion.identity);
+        Instantiate(dropPrefab, spawnPoint9.position, Quaternion.identity);
     }
 }
