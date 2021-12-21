5f721d10ede592669e3f6addf7aaa197e3f64c97
ksui01 <suistem5@gmail.com>
2021-12-21 14:01:04 -0500

    linked slider to sound

--- Rats!!/Assets/Scripts/VolumeSettings.cs
+++ Rats!!/Assets/Scripts/VolumeSettings.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+    
+    public void SetVolume (float volume)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        audioMixer.SetFloat("volume", volume);
     }
 }
