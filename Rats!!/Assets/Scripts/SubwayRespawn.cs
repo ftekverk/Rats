72bc844d3f15840fc731683377da104a566f43a6
ksui01 <suistem5@gmail.com>
2021-12-21 14:50:50 -0500

    pause change, level5 -> end screen change, more water drop changes

---
+++ Rats!!/Assets/Scripts/SubwayRespawn.cs
@@ -0,0 +1,51 @@
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubwayRespawn : MonoBehaviour
+{
+    public Transform pSpawn;       // current player spawn point
+    public PlayerHealth healthscript;
+    bool reset_level = true;
+    public Vector3 pSpn2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (healthscript.numHealth <= 0)
+        {
+            if (reset_level)
+            {
+                SceneManager.LoadScene("FlowingWater");
+            }
+            if (pSpawn != null)
+            {
+                pSpn2 = new Vector3(pSpawn.position.x, pSpawn.position.y, transform.position.z);
+                gameObject.transform.position = pSpn2;
+                healthscript.numHealth = 3;
+                healthscript.healthBar[0].SetActive(true);
+                healthscript.healthBar[1].SetActive(true);
+                healthscript.healthBar[2].SetActive(true);
+            }
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            pSpawn = other.gameObject.transform;
+            GameObject thisCheckpoint = other.gameObject;
+            reset_level = false;
+            //StopCoroutine(changeColor(other));
+            // StartCoroutine(changeColor(other));
+        }
+    }
+}
